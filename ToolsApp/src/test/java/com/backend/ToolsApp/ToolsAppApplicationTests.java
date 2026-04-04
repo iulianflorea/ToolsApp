1e5136fa17beb47145ec89020e7ed9559719548a
Iulian Florea <iulian.florea4@gmail.com>
2026-04-04 09:28:52 +0300

    initializare git cu github

---
+++ ToolsApp/src/test/java/com/backend/ToolsApp/ToolsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ToolsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
